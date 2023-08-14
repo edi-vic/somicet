b5b3c13dd12ba1bae2fc2be92e7481c6bee3e0f5
Víctor Peña Romero <vic.pero@icloud.com>
2023-08-14 11:59:06 -0600

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vue from "@astrojs/vue";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [vue()]
+  integrations: [vue()],
+  adapter: netlify()
 });
