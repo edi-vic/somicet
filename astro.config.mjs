e4ee0750ad9b1aa3832d077e0c3d9c541a51dde8
Víctor Peña Romero <vic.pero@icloud.com>
2023-08-14 13:24:10 -0600

    clean project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [vue()],
