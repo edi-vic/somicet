3bb3f2509cb502d8ca43aca374bc89f29f44e2ac
Víctor Peña Romero <vic.pero@icloud.com>
2023-08-16 08:01:18 -0600

    fix profile call

--- src/components/register/RegisterModule.vue
+++ src/components/register/RegisterModule.vue
@@ -7,22 +7,25 @@
     v-if="step === STEPS[1]"
     @success="handleNextStep"
   />
-  <!-- <NameStep/>
-  <ReceiptStep /> -->
+  <NameStep v-if="step === STEPS[2]"/>
 </template>
 
 <script setup>
 import EmailStep from '@components/register/00EmailStep.vue'
 import OtpStep from '@components/register/01OtpStep.vue'
 import NameStep from '@components/register/NameStep.vue'
-import ReceiptStep from '@components/register/ReceiptStep.vue'
 import { ref, reactive, onMounted } from 'vue'
 import { session } from '@stores/session'
 import { supabase } from '@helpers/supabase'
 import { STEPS } from '@helpers/constants'
 
 /*  vue  state  */
-const step = ref(STEPS[0])
+const step = ref(null)
+const status = reactive({
+  loading: false,
+  success: false,
+  error: null,
+})
 
 /*  vue  lifecycle  */
 onMounted(async () => {
@@ -39,6 +42,10 @@ const getUserProfile = async () => {
     return
   }
 
+  status.loading = true
+  status.success = false
+  status.error = null
+
   const { data, error } = await supabase
     .from('profiles')
     .select()
@@ -48,8 +55,20 @@ const getUserProfile = async () => {
   if (error) {
     console.error('Error in getUserProfile: ', error)
   } else {
-    console.log('DATA ', data)
+    const { id, email, first_name, last_name } = data
+    session.set({ 
+      user_id: id,
+      user_email: email,
+    })
+    if (!first_name || !last_name) {
+      step.value = STEPS[2]
+    } else {
+      step.value = STEPS[3]
+    }
+    status.success = true
   }
+
+  status.loading = false
 }
 
 const handleNextStep = (val) => step.value = val
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,4 +1,6 @@
 export const STEPS = {
   0: 'no_user',
   1: 'otp',
+  2: 'user_name',
+  3: 'receipt',
 }
