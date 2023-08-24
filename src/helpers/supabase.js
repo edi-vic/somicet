2be3cacbe82875fd309c8ecd6525ce425bb245e4
Víctor Peña Romero <vic.pero@icloud.com>
2023-08-24 07:18:40 -0600

    adjustments in admin auth

--- src/helpers/supabase.js
+++ src/helpers/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 const sbUrl = import.meta.env.PUBLIC_SUPABASE_URL
 const sbKey = import.meta.env.PUBLIC_SUPABASE_KEY
@@ -9,7 +9,22 @@ export const supabase = createClient(
 )
 
 export async function getUser(req) {
-  console.log('getUser', req)
+  const cookie = req.headers.get("cookie");
+  const match = cookie.match(/sbat=([^;]+)/);
+  const sbat = match?.[1];
+  if (!sbat) return null;
+
+  const { data: { user } } = await supabase.auth.getUser(sbat);
+  if (!user || user.role !== "authenticated") return null;
+  return user;
 }
 
-export const isLoggedIn = async () => getUser() != null;
+export async function getUserRole(user) {
+  const { data, error } = await supabase
+    .from('profiles')
+    .select()
+    .eq('user_id', user.id)
+    .single();
+
+    console.log(data, error)
+}
