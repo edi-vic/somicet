01f3d73e858a7bceb1dc2bb1c774e2b002d56bec
Víctor Peña Romero <vic.pero@icloud.com>
2023-08-31 21:57:01 -0600

    finish poster states

--- src/components/registration/06PosterStep.vue
+++ src/components/registration/06PosterStep.vue
@@ -1,44 +1,44 @@
 <template>
   <section
-    class="project-step project-step--loading"
+    class="poster-step poster-step--loading"
     v-if="status.loading"
   >
     <Loader />
   </section>
 
   <section 
-    class="project-step"
-    v-if="!status.loading"
+    class="poster-step"
+    v-if="!status.loading && !poster.id"
   >
-    <p class="project-step__p">
+    <p class="poster-step__p">
       Es esencial completar todos los campos del formulario. El resumen 
       debe ser claro, conciso y no exceder de 250 palabras. Todo trabajo
       será revisado por el comité y está sujeto a aceptación.
       Revisa cuidadosamente toda la información antes de enviar.
     </p>
     <label
-      class="project-step__label"
+      class="poster-step__label"
       for="title"
     >
       Título del proyecto
     </label>
     <input
       id="title"
-      class="project-step__input"
+      class="poster-step__input"
       type="text"
       placeholder="Título"
-      v-model="project.title"
+      v-model="poster.title"
     />
 
     <label
-      class="project-step__label"
+      class="poster-step__label"
       for="group"
     >
       Grupo
     </label>
     <select
-      class="project-step__input"
-      v-model="project.theme"
+      class="poster-step__input"
+      v-model="poster.theme"
     >
     <option
         v-for="option in posterThemes"
@@ -51,69 +51,166 @@
     </select>
 
     <label
-      class="project-step__label"
+      class="poster-step__label"
       for="authors"
     >
       Autores
     </label>
     <input
       id="authors"
-      class="project-step__input"
+      class="poster-step__input"
       type="text"
       placeholder="Autores"
-      v-model="project.authors"
+      v-model="poster.authors"
     />
     <label
-      class="project-step__label"
+      class="poster-step__label"
       for="secondment"
     >
       Adscripción
     </label>
     <input
       id="secondment"
-      class="project-step__input"
+      class="poster-step__input"
       type="text"
       placeholder="Adscripción"
-      v-model="project.secondment"
+      v-model="poster.secondment"
     />
     <label
-      class="project-step__label"
+      class="poster-step__label"
       for="summary"
     >
       Resumen
     </label>
     <textarea
       id="summary"
-      class="project-step__textarea project-step__textarea--summary"
+      class="poster-step__textarea poster-step__textarea--summary"
       placeholder="Resumen"
-      v-model="project.summary"
+      v-model="poster.summary"
     />
     <p 
-      class="project-step__p project-step__p--small"
-      :class="{ 'project-step__p--error': wordCount > 250 }"
+      class="poster-step__p poster-step__p--small"
+      :class="{ 'poster-step__p--error': wordCount > 250 }"
     >
       Palabras: {{ wordCount }} / 250
     </p>
     <label
-      class="project-step__label"
+      class="poster-step__label"
       for="acknowledgements"
     >
       Agradecimientos
     </label>
     <textarea
       id="acknowledgements"
-      class="project-step__textarea"
+      class="poster-step__textarea"
       placeholder="Agradecimientos"
-      v-model="project.acknowledgements"
+      v-model="poster.acknowledgements"
     />
     <button
-      class="project-step__button"
+      class="poster-step__button"
       @click="savePoster"
       :disabled="status.loading || !isFormComplete"
     >
       Enviar
     </button>
   </section>
+
+  <section 
+    class="poster-step"
+    v-if="!status.loading && poster.id && poster.status === 'pending'"
+  >
+    <h4 class="poster-step__title">
+      Revisión de Cartel en Proceso
+    </h4>
+    <p class="poster-step__p">
+      ¡Gracias por enviar tu cartel! Actualmente está en revisión por 
+      nuestro comité. Te notificaremos el resultado por correo electrónico 
+      a más tardar el 29 de septiembre de 2023. Agradecemos tu paciencia.
+    </p>
+  </section>
+
+  <section 
+    class="poster-step"
+    v-if="!status.loading && poster.id && poster.status === 'approved'"
+  >
+    <h4 class="poster-step__title">
+      ¡Tu cartel ha sido seleccionado!
+    </h4>
+    <p class="poster-step__p">
+      Nos complace informarte que, tras el proceso de evaluación, 
+      tu trabajo ha sido seleccionado para participar en el Simposio 
+      "Células Troncales, Medicina Regenerativa y Envejecimiento".
+    </p>
+    <p class="poster-step__p">
+      Tu número de póster es {{ poster.approval_number }} y deberás presentarte el día 
+      {{ poster.presentation_date }} a partir de las {{ poster.presentation_time }}.
+    </p>
+    <p class="poster-step__p">
+      A continuación, te listamos los requerimientos para la 
+      presentación de tu proyecto:
+    </p>
+    <ol>
+      <li class="poster-step__element">
+        <div class="poster-step__instruction">
+          <span class="poster-step__bullet">a</span>
+          <p class="poster-step__p">
+            Las medidas máximas son 150 cm de alto por 100 cm de ancho.
+          </p>
+        </div>
+      </li>
+      <li class="poster-step__element">
+        <div class="poster-step__instruction">
+          <span class="poster-step__bullet">b</span>
+          <p class="poster-step__p">
+            El cartel debe contener la siguiente información:
+          </p>
+        </div>
+        <ol class="poster-step__sub-list">
+          <li class="poster-step__sub-element">
+            Resumen
+          </li>
+          <li class="poster-step__sub-element">
+            Introducción
+          </li>
+          <li class="poster-step__sub-element">
+            Metodología y Resultados
+          </li>
+          <li class="poster-step__sub-element">
+            Conclusiones y perspectivas
+          </li>
+          <li class="poster-step__sub-element">
+            Agradecimientos
+          </li>
+        </ol>
+      </li>
+    </ol>
+  </section>
+
+  <section 
+    class="poster-step"
+    v-if="!status.loading && poster.id && poster.status === 'rejected'"
+  >
+    <h4 class="poster-step__title">
+      Cartel no seleccionado
+    </h4>
+    <p class="poster-step__p">
+      Lamentamos informarte que, tras un cuidadoso proceso de evaluación, 
+      tu trabajo no fue seleccionado para participar en el Simposio "Células 
+      Troncales, Medicina Regenerativa y Envejecimiento".
+    </p>
+    <p class="poster-step__p">
+      Aunque tu propuesta no fue seleccionada en esta ocasión, agradecemos 
+      sinceramente tu interés en el Simposio y te invitamos a participar 
+      como asistente, lo que también te permitirá obtener una constancia.
+    </p>
+    <p class="poster-step__p">
+      Si deseas solicitar la devolución de tu pago de inscripción, por favor 
+      envía un correo a somicet@gmail.com adjuntando tu comprobante de pago 
+      y la cuenta a la que deseas que se realice la devolución, siempre y 
+      cuando no hayas solicitado factura.
+    </p>
+  </section>
+
 </template>
 
 <script setup>
@@ -132,7 +229,7 @@ const { registration } = defineProps({
 })
 
 /*  vue  state  */
-const project = ref({
+const poster = ref({
   title: "",
   authors: "",
   theme: "no_theme",
@@ -148,13 +245,13 @@ const status = reactive({
 
 /*  vue  lifecycle  */
 onMounted(() => {
-  getProject()
+  getPoster()
 })
 
 /*  vue  computed  */
 const posterThemes = computed(() => Object.values(POSTER_THEMES))
 const wordCount = computed(() => {
-  const summary = project.value.summary;
+  const summary = poster.value.summary;
   if (!summary) {
     return 0;
   }
@@ -163,17 +260,17 @@ const wordCount = computed(() => {
 });
 
 const isFormComplete = computed(() => {
-  return !isEmpty(project.value.title)
-    && !isEmpty(project.value.authors)
-    && project.value.theme !== "no_theme"
-    && !isEmpty(project.value.secondment)
-    && !isEmpty(project.value.summary)
+  return !isEmpty(poster.value.title)
+    && !isEmpty(poster.value.authors)
+    && poster.value.theme !== "no_theme"
+    && !isEmpty(poster.value.secondment)
+    && !isEmpty(poster.value.summary)
     && wordCount.value <= 250
-    && !isEmpty(project.value.acknowledgements)
+    && !isEmpty(poster.value.acknowledgements)
 })
 
 /*  vue  methods  */
-const getProject = async () => {
+const getPoster = async () => {
   status.loading = true
   status.success = false
   status.error = null
@@ -185,10 +282,10 @@ const getProject = async () => {
     .single()
 
   if (error) {
-    console.error("Error in getProject: ", error)
+    console.error("Error in getPoster: ", error)
   } else {
-    console.log("getProject: ", data)
-    project.value = data
+    console.log("getPoster: ", data)
+    poster.value = data
   }
 
   status.loading = false
@@ -201,12 +298,12 @@ const savePoster = async () => {
 
   const record = {
     "user_id": registration.user_id,
-    "title": project.value.title,
-    "authors": project.value.authors,
-    "theme": project.value.theme,
-    "secondment": project.value.secondment,
-    "summary": project.value.summary,
-    "acknowledgements": project.value.acknowledgements,
+    "title": poster.value.title,
+    "authors": poster.value.authors,
+    "theme": poster.value.theme,
+    "secondment": poster.value.secondment,
+    "summary": poster.value.summary,
+    "acknowledgements": poster.value.acknowledgements,
     "registration_id": registration.id,
   }
 
@@ -218,9 +315,9 @@ const savePoster = async () => {
 
 
   if (error) {
-    console.error("Error in savePosters: ", error)
+    console.error("Error in savePoster: ", error)
   } else {
-    console.log("savePosters: ", data)
+    console.log("savePoster: ", data)
     status.success = true
   }
 
@@ -230,7 +327,7 @@ const savePoster = async () => {
 
 <style scoped lang="scss">
 @import "@assets/library";
-.project-step {
+.poster-step {
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -238,6 +335,11 @@ const savePoster = async () => {
   &--loading {
     align-items: center;
     height: 300px;
+  }
+  &__title {
+    color: $primary-color;
+    font-size: 20px;
+    margin-bottom: 12px;
   }
   &__p {
     font-size: 16px;
@@ -290,6 +392,37 @@ const savePoster = async () => {
     &:disabled {
       background-color: $primary-color-600;
       cursor: not-allowed;
+    }
+  }
+  &__element {
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 12px;
+  }
+  &__instruction {
+    display: flex;
+    color: $black;
+  }
+  &__bullet {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: $primary-color;
+    color: $white;
+    flex-shrink: 0;
+    margin-right: 8px;
+  }
+  &__sub-list {
+    width: 80%;
+    margin-left: 10%;
+  }
+  &__sub-element {
+    margin-bottom: 8px;
+    &::marker {
+      color: $primary-color;
     }
   }
   @media (min-width: 992px) {
